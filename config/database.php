0262bafb9f76a0b1b4fc07c132d8ad47488ec0ae
aminrais86 <serahku86@gmail.com>
2019-09-05 13:42:43 +0800

    Amin

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('login', 'mysql'),
+    'default' => env('amin', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'login'),
+            'database' => env('DB_DATABASE', 'amin'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
